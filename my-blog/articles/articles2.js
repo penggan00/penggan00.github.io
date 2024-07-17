132eee15e69c71242a74af474d3770da258658a1
好酒 <106035465+penggan00@users.noreply.github.com>
2024-07-17 15:44:56 +0800

    2024.7.17.4

--- my-blog/articles/articles2.js
+++ my-blog/articles/articles2.js
@@ -1,12 +1,12 @@
 const articles2 = [
-    { title: "linux命令大全", link: "articles/_posts/36.html", lastModified: "2024-06-29 00:41:00" },
-    { title: "不良林节点搭建教程", link: "articles/_posts/37.html", lastModified: "2024-07-01 00:41:00" },
-    { title: " 京东云AX1800亚瑟", link: "articles/_posts/38.html", lastModified: "2024-07-10 00:41:00" },
-    { title: "s905 EmuELEC", link: "articles/_posts/39.html", lastModified: "2024-07-13 00:41:00" },
-    { title: "各种智能电视开启ADB", link: "articles/_posts/40.html", lastModified: "2024-07-13 00:41:00" },
-    { title: "安卓adb命令", link: "articles/_posts/41.html", lastModified: "2024-07-13 00:41:00" },
-    { title: "常用的HTML标签", link: "articles/_posts/42.html", lastModified: "2024-07-15 00:41:00" },
-    { title: "GitHub Actions 编译实例", link: "articles/_posts/43.html", lastModified: "2024-07-17 00:41:00" },
+    { title: "linux命令大全", link: "articles/post/36.html", lastModified: "2024-06-29 00:41:00" },
+    { title: "不良林节点搭建教程", link: "articles/post/37.html", lastModified: "2024-07-01 00:41:00" },
+    { title: " 京东云AX1800亚瑟", link: "articles/post/38.html", lastModified: "2024-07-10 00:41:00" },
+    { title: "s905 EmuELEC", link: "articles/post/39.html", lastModified: "2024-07-13 00:41:00" },
+    { title: "各种智能电视开启ADB", link: "articles/post/40.html", lastModified: "2024-07-13 00:41:00" },
+    { title: "安卓adb命令", link: "articles/post/41.html", lastModified: "2024-07-13 00:41:00" },
+    { title: "常用的HTML标签", link: "articles/post/42.html", lastModified: "2024-07-15 00:41:00" },
+    { title: "GitHub Actions 编译实例", link: "articles/post/43.html", lastModified: "2024-07-17 00:41:00" },
     
 
 
