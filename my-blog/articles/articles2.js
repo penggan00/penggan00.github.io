03befc5d89b25392ac57c85b76d85465817a13bf
好酒 <106035465+penggan00@users.noreply.github.com>
2024-08-02 13:22:52 +0800

    2024.8.2

--- my-blog/articles/articles2.js
+++ my-blog/articles/articles2.js
@@ -5,8 +5,7 @@ const articles2 = [
     { title: " android4.4.2 tv", link: "articles/post/53.html", lastModified: "2024-07-27 01:55:00" },
     { title: "  sing-box", link: "articles/post/54.html", lastModified: "2024-07-29 13:55:00" },
     { title: "Win SingBox", link: "articles/post/55.html", lastModified: "2024-07-29 13:55:00" },
-    
+    { title: "Cloudflare Worker", link: "articles/post/56.html", lastModified: "2024-08-02 13:55:00" },
    
-
     
 ];
