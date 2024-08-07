167a8c13ee2f0b08a6ba071f9ec3bfb3e4593f14
好酒 <106035465+penggan00@users.noreply.github.com>
2024-08-08 00:33:14 +0800

    2024.8.8

--- my-blog/articles/articles2.js
+++ my-blog/articles/articles2.js
@@ -1,11 +1,11 @@
 const articles2 = [
-    { title: "windows系统充当软路由", link: "articles/post/50.html", lastModified: "2024-07-24 22:53:00" },
+    { title: "微软HEVC视频安装", link: "articles/post/50.html", lastModified: "2024-07-24 22:53:00" },
     { title: "Telegram群组", link: "articles/post/51.html", lastModified: "2024-07-24 22:55:00" },
     { title: " 解锁流媒体、chatgpt", link: "articles/post/52.html", lastModified: "2024-07-27 01:55:00" },
     { title: " android4.4.2 tv", link: "articles/post/53.html", lastModified: "2024-07-27 01:55:00" },
     { title: "  sing-box", link: "articles/post/54.html", lastModified: "2024-07-29 13:55:00" },
     { title: "Win SingBox", link: "articles/post/55.html", lastModified: "2024-07-29 13:55:00" },
     { title: "Cloudflare Worker", link: "articles/post/56.html", lastModified: "2024-08-02 13:55:00" },
-   
+    { title: "Cloudflare Worker", link: "articles/post/57.html", lastModified: "2024-08-08 13:55:00" },
     
 ];
