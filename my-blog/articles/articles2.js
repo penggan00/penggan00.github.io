3094044e28d4c0e04f3c29989617c263a56b95fd
好酒 <106035465+penggan00@users.noreply.github.com>
2024-08-12 10:42:28 +0800

    2024.8.12

--- my-blog/articles/articles2.js
+++ my-blog/articles/articles2.js
@@ -7,8 +7,7 @@ const articles2 = [
     { title: "Win SingBox", link: "articles/post/55.html", lastModified: "2024-07-29 13:55:00" },
     { title: "Cloudflare Worker", link: "articles/post/56.html", lastModified: "2024-08-02 13:55:00" },
     { title: "刷机OnePlus 5", link: "articles/post/57.html", lastModified: "2024-08-10 13:55:00" },
+    { title: "通杀老U盘量产", link: "articles/post/58.html", lastModified: "2024-08-11 22:55:00" },
 
-
-
-
+    
 ];
